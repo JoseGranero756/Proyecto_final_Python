ccb2b1718d198055749f588a28bacfdd759ac6d4
Jose Granero <josegranero756@gmail.com>
2023-08-01 21:02:25 -0300

    creada clase VistaReview y clase ControladorReview

--- controllers/controlador_info.py
+++ controllers/controlador_info.py
@@ -2,8 +2,14 @@ class ControladorInfo:
     def __init__(self,app):
         self.app = app
     
+    def hacer_review(self,evento):
+        self.app.vista_review.hacer_review(evento)
+        self.app.cambiar_frame(self.app.vista_review)
+        
+    
     def volver_inicio(self):
         self.app.cambiar_frame(self.app.vista_inicio)
+        
     
     
     
--- views/vista_review.py
+++ views/vista_review.py
@@ -2,5 +2,18 @@ import tkinter as tk
 from tkinter.font import Font
 class VistaReview(tk.Frame):
     def __init__(self,master = None, controlador = None):
-        pass
+        """
+        Crea la vista de reviews
+        """
+        
+        super().__init__(master)
+        self.master = master
+        self.controlador = controlador
+        self.evento = None
+        
+    
+        
+    def hacer_review(self,evento):
+        self.evento = evento
+        
     
